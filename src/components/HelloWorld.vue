40d0815a3a68c2d1cacbccc9d17d983311da831a
Thirty33 <joelsuarez.1101@gmail.com>
2017-11-13 01:05:17 -0500

    puntos de anclaje de directivas personalizadas en vue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1 v-fijar:fondo.peru="90">Elemeto estatico</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores nemo laborum fugit repudiandae harum illum magni porro consequatur dignissimos recusandae adipisci ut a, nesciunt odio accusantium modi, voluptas, veritatis cum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores nemo laborum fugit repudiandae harum illum magni porro consequatur dignissimos recusandae adipisci ut a, nesciunt odio accusantium modi, voluptas, veritatis cum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores nemo laborum fugit repudiandae harum illum magni porro consequatur dignissimos recusandae adipisci ut a, nesciunt odio accusantium modi, voluptas, veritatis cum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores nemo laborum fugit repudiandae harum illum magni porro consequatur dignissimos recusandae adipisci ut a, nesciunt odio accusantium modi, voluptas, veritatis cum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores nemo laborum fugit repudiandae harum illum magni porro consequatur dignissimos recusandae adipisci ut a, nesciunt odio accusantium modi, voluptas, veritatis cum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores nemo laborum fugit repudiandae harum illum magni porro consequatur dignissimos recusandae adipisci ut a, nesciunt odio accusantium modi, voluptas, veritatis cum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores nemo laborum fugit repudiandae harum illum magni porro consequatur dignissimos recusandae adipisci ut a, nesciunt odio accusantium modi, voluptas, veritatis cum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores nemo laborum fugit repudiandae harum illum magni porro consequatur dignissimos recusandae adipisci ut a, nesciunt odio accusantium modi, voluptas, veritatis cum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores nemo laborum fugit repudiandae harum illum magni porro consequatur dignissimos recusandae adipisci ut a, nesciunt odio accusantium modi, voluptas, veritatis cum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores nemo laborum fugit repudiandae harum illum magni porro consequatur dignissimos recusandae adipisci ut a, nesciunt odio accusantium modi, voluptas, veritatis cum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores nemo laborum fugit repudiandae harum illum magni porro consequatur dignissimos recusandae adipisci ut a, nesciunt odio accusantium modi, voluptas, veritatis cum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores nemo laborum fugit repudiandae harum illum magni porro consequatur dignissimos recusandae adipisci ut a, nesciunt odio accusantium modi, voluptas, veritatis cum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores nemo laborum fugit repudiandae harum illum magni porro consequatur dignissimos recusandae adipisci ut a, nesciunt odio accusantium modi, voluptas, veritatis cum.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,40 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+// directivas propias
+Vue.directive('fijar', {
+	bind(el, binding) {
+		console.log(el,binding)
+		el.style.position = 'fixed'
+		// expresiones
+		if(binding.expression) {
+			el.style.top = binding.expression + 'px'
+		}
+		// argumentos
+		if(binding.arg) {
+			let color = 'peru'
+			let modificadores = Object.keys(binding.modifiers)
+			// modificadores
+			if(modificadores.length > 0) {
+				color = modificadores[0]
+			}
+			el.style.backgroundColor = color
+		}
+
+		// modificadores
+
+	}
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
